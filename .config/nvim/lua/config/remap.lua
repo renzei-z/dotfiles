172d6485f4bbcc3422a6bd39d06492ebbe666e86
Basil <cinnabasil@protonmail.com>
2024-03-21 01:51:49 +0000

    feat: nvim remaps and vars

--- .config/nvim/lua/config/remap.lua
+++ .config/nvim/lua/config/remap.lua
@@ -1,2 +1,31 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- Move highlighted text with J and K
+vim.keymap.set('v', 'J', ":m '<-2<CR>gv=gv")
+vim.keymap.set('v', 'K', ":m '>+1<CR>gv=gv")
+
+vim.keymap.set('n', 'J', 'mzJ`z')
+vim.keymap.set('n', '<C-d>', '<C-d>zz')
+vim.keymap.set('n', '<C-u>', '<C-u>zz')
+
+-- Keep search terms in center of screen when going through them
+vim.keymap.set('n', 'n', 'nzzzv')
+vim.keymap.set('n', 'N', 'Nzzzv')
+
+-- Paste over text without copying deleted text
+vim.keymap.set('x', '<leader>p', "\"_dP")
+
+-- Copy to clipboard with yank
+vim.keymap.set('n', '<leader>y', '\"+y')
+vim.keymap.set('v', '<leader>y', '\"+y')
+vim.keymap.set('n', '<leader>Y', '\"+Y')
+
+vim.keymap.set('n', '<leader>d', '\"_d')
+vim.keymap.set('v', '<leader>d', '\"_d')
+
+vim.keymap.set('n', 'Q', '<nop>')
+vim.keymap.set('n', '<leader>f', function() vim.lsp.buf.format() end)
+
+-- Find and replace currently hovered word
+vim.keymap.set('n', '<leader>s', ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>")
